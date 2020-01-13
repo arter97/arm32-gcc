512c928da0631257c4501d4a721dd74518bd95ab
Park Ju Hyung <qkrwngud825@gmail.com>
2020-01-13 22:15:29 +0900

    Rebuild to statically link GMP/MPFR/MPC/ISL
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Change-Id: Icf80d0b6cc8f99c7861b734415345581af4b5e5c

--- lib/gcc/arm-eabi/9.2.0/plugin/include/tm-preds.h
+++ lib/gcc/arm-eabi/9.2.0/plugin/include/tm-preds.h
@@ -1,5 +1,5 @@
 /* Generated automatically by the program 'build/genpreds'
-   from the machine description file '../../linaro/gcc/config/arm/arm.md'.  */
+   from the machine description file '../../gcc/gcc/config/arm/arm.md'.  */
 
 #ifndef GCC_TM_PREDS_H
 #define GCC_TM_PREDS_H
